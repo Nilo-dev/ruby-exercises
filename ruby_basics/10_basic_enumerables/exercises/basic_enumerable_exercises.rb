a7d2a7dc47d0e307dab9abedde38a409e18cba79
nilo-dev <danilo.dalo.nilo@gmail.com>
2024-10-08 14:19:07 -0300

    Cleaning some code practices

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def display_current_inventory(inventory_list)
   # use #each to iterate through each item of the inventory_list (a hash)
   # use puts to output each list item "<key>, quantity: <value>" to console
